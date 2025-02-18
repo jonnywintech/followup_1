374a5f2ad543ed452e29895a1e5739692c783f33
Nikola <jonnywintech@gmail.com>
2025-02-18 21:20:23 +0100

    adding initial controlelrs and models

---
+++ database/seeders/FormDataSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FormDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
